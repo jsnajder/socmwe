a67dffc0c7e1fd4f29e55afd728ad43eedfc1f2c
Jan Snajder <jan.snajder@fer.hr>
2016-07-22 16:03:46 +1000

    Initial commit

--- src/filter_tweets.py
+++ src/filter_tweets.py
@@ -1,1 +1,31 @@
 
+import json
+import subprocess
+
+
+def read_tweets(filename, decompress=None, filter=None):
+    '''
+    Reads in tweets. Expects one JSON tweet per line.
+    Returns a list of dictionaries.
+    '''
+    if decompress == 'lzo':
+        p = subprocess.Popen(['lzop', '-dc', filename], stdout=subprocess.PIPE)
+        for line in p.stdout.readlines():
+            tweet = json.loads(line)
+            if filter is not None and not filter(tweet):
+                continue
+            else:
+                yield tweet
+        p.wait()
+    else:
+        with open(filename) as f:
+            for line in f:
+                tweet = json.loads(line)
+                if filter is not None and not filter(tweet):
+                    continue
+                else:
+                    yield tweet
+
+
+def filter_lang(lang):
+    return (lambda tweet: tweet.get('lang', None) == lang)
