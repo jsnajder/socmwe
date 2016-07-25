fb07fde5261fa4e160477cd065bf6e3607a844f7
Jan Snajder <jan.snajder@fer.hr>
2016-07-25 18:31:08 +1000

    Add corpus extraction

---
+++ src/extract_corpus.py
@@ -0,0 +1,37 @@
+
+import subprocess
+import sys
+
+
+def readlines_lzo(filename):
+    p = subprocess.Popen(['lzop', '-dc', filename], stdout=subprocess.PIPE)
+    for line in p.stdout.readlines():
+        yield line
+    p.wait()
+
+
+ident = 0
+lang1 = 4
+location = 11
+lang2 = 12
+text = 13
+
+
+def country_code(s):
+    return s.split('-')[-1] if s != 'None' else None
+
+
+def main():
+    f = sys.argv[1]
+    for l in readlines_lzo(f):
+        x = l.split('\t')
+        lang1 = x[4]
+        lang2 = x[12]
+        country = country_code(x[11])
+        if lang1 == 'en' and lang2 == 'en' and country is not None:
+            s = '\t'.join((x[0], country, x[13]))
+            sys.stdout.write(s)
+    sys.stdout.flush()
+
+if __name__ == "__main__":
+    main()
