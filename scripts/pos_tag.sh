dbba1418ef492ea35da03eab293501fb70f762cd
Jan Snajder <snajder@ray.cis.unimelb.edu.au>
2016-08-02 10:44:25 +1000

    Fix hashtag bug in lemmatization

--- scripts/pos_tag.sh
+++ scripts/pos_tag.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-data="../../data"
+data="../data"
 tagger="../../ark-tweet-nlp/ark-tweet-nlp-0.3.2/runTagger.sh"
 
-nohup $tagger --no-confidence --input-format text --output-format conll --input-field 3 $data/tweet-country-text.txt > $data/tweets.pos.txt &
+for batch in {00..03}; do
+  nohup $tagger --no-confidence --input-format text --output-format conll --input-field 3 $data/tweets.filtered.txt.$batch > $data/tweets.pos.txt.$batch &
+done
