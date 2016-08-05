4476b0485c952f89b26fc59a8017c80687cb8506
Jan Snajder <jan.snajder@fer.hr>
2016-08-05 14:25:13 +1000

    Add align_outputs

--- src/align_outputs.py
+++ src/align_outputs.py
@@ -1,5 +1,35 @@
 
 import sys
+import re
+import htmlentitydefs
+
+##
+# Removes HTML or XML character references and entities from a text string.
+#
+# @param text The HTML (or XML) source text.
+# @return The plain text, as a Unicode string, if necessary.
+
+
+def unescape(text):
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text  # leave as is
+    return re.sub("&#?\w+;", fixup, text)
 
 
 def main():
@@ -11,7 +41,9 @@ def main():
         for l1 in f_txt:
             line1 += 1
             x1 = l1.split('\t')
-            t1 = x1[2].strip().decode('utf-8').replace(' ', '').replace(u'\u00a0', '').encode('utf-8')
+            t1 = x1[2].strip().decode('utf-8').replace(' ', '').\
+                replace(u'\u00a0', '').encode('utf-8')
+            t1 = unescape(t1)
             t2 = ''
             for l2 in f_conll:
                 line2 += 1
